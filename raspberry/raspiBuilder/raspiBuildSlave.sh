19532a42f8157cb188403944949747f68bc99c1a
icebear8 <ebert.dominik@gmail.com>
2016-06-07 21:29:28 +0200

    Improve raspberry image builder image size

--- raspberry/raspiBuilder/raspiBuildSlave.sh
+++ raspberry/raspiBuilder/raspiBuildSlave.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+git clone git://github.com/raspberrypi/tools.git --depth 1 ./src
+mkdir -p ./Dockerfiles/workspace/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/
+cp -rf ./src/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/ ./Dockerfiles/workspace/arm-bcm2708/
+
 # Builds a cross compiler for raspberry pi
 # -t defines the name for the created images
-
 docker build -t biz/raspislave ./Dockerfiles
+
+rm -rf ./Dockerfiles/workspace
+rm -rf ./src
