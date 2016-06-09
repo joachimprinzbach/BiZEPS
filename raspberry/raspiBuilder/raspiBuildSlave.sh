d78d7b81e5d8e235a7b5a1bf79eb8dfd4b05e4db
icebear8 <ebert.dominik@gmail.com>
2016-06-09 19:54:03 +0200

    Beautify shell comments

--- raspberry/raspiBuilder/raspiBuildSlave.sh
+++ raspberry/raspiBuilder/raspiBuildSlave.sh
@@ -2,17 +2,28 @@
 
 # Clone the project on the host and not within the dockerfile
 # This keeps the generated docker image smaller
-echo "Download resources"
+echo
+echo "PREPARE: Download resources for docker image"
+echo "-----------------------------------"
 git clone git://github.com/raspberrypi/tools.git --depth 1 ./src
 mkdir -p ./Dockerfiles/workspace/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/
 cp -rf ./src/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/ ./Dockerfiles/workspace/arm-bcm2708/
 
 # Builds a cross compiler for raspberry pi
 # -t defines the name for the created images
-echo "Create docker image"
+echo
+echo "EXECUTE: Create docker image"
+echo "-------------------"
 docker build -t biz/raspislave ./Dockerfiles
 
 # Remove the resources from the host after the docker image has been built
-echo "Remove resources"
+echo
+echo "WRAP UP: Remove no more used resources"
+echo "-----------------------------"
 rm -rf ./src
 rm -rf ./Dockerfiles/workspace
+
+echo
+echo "----"
+echo "DONE"
+
