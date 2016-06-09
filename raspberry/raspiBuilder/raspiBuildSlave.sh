41e076ccd9446c5d4a9b9c7292a61ce46b6617fe
icebear8 <ebert.dominik@gmail.com>
2016-06-09 19:42:31 +0200

    Added build comments

--- raspberry/raspiBuilder/raspiBuildSlave.sh
+++ raspberry/raspiBuilder/raspiBuildSlave.sh
@@ -2,14 +2,17 @@
 
 # Clone the project on the host and not within the dockerfile
 # This keeps the generated docker image smaller
+echo "Download resources"
 git clone git://github.com/raspberrypi/tools.git --depth 1 ./src
 mkdir -p ./Dockerfiles/workspace/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/
 cp -rf ./src/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/ ./Dockerfiles/workspace/arm-bcm2708/
 
 # Builds a cross compiler for raspberry pi
 # -t defines the name for the created images
+echo "Create docker image"
 docker build -t biz/raspislave ./Dockerfiles
 
 # Remove the resources from the host after the docker image has been built
+echo "Remove resources"
 rm -rf ./src
 rm -rf ./Dockerfiles/workspace
