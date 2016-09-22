abe81e76e697999f699779fcf8a095285107d687
Daniel Merz <kogi@gmx.ch>
2016-09-22 16:39:47 +0200

    build jenkins Home

--- buildServer/jenkins/buildImage.sh
+++ buildServer/jenkins/buildImage.sh
@@ -4,5 +4,6 @@
 # -t defines the name for the created images
 
 docker build -t biz/jenkinsuser ./user
+docker build -t biz/jenkinshome ./home --build-arg DOCKER_SERVER_URL=tcp://192.168.99.100:2376
 docker build -t biz/jenkins ./master
 docker build -t biz/jenkinsslave ./slave
